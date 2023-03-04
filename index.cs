29b2c250a9c9f05513090b150e851f4202878994
carp51 <103875907+carp51@users.noreply.github.com>
2023-03-05 00:56:49 +0900

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,13 @@
+#myInput {
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  width: 40%; /* 幅を指定 */
+  margin-top: 50px;
+}
+
+#myDiv {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+}
